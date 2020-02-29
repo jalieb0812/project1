971b859bb8a20104f7fa1463c4375df57186d2f1
Jordan <jalieb0812@gmail.com>
2020-02-29 18:08:41 -0500

    commit on 2-29-2020

--- import.py
+++ import.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+#function for importing book info into databse
 def main():
     f = open("books.csv")
     reader = csv.reader(f)
